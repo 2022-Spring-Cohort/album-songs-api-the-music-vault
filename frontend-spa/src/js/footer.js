d5a3a029b137858cb45cf4b4b64f2d24cfdafbe8
AveryCS <Averydcs@gmail.com>
2022-03-17 15:22:18 -0400

    created js files. Will pick back up on the app.js file tomorrow to begin making fetch

---
+++ frontend-spa/src/js/footer.js
@@ -0,0 +1,7 @@
+export default function header(){
+    return  `<footer class="footer">
+    <p>Copyright &copy;
+        <script>document.write(new Date().getFullYear())</script> The Music Vault, WCCI
+    </p>
+</footer>`
+}
---
+++ frontend-spa/src/js/home.js
@@ -0,0 +1,26 @@
+export default function home(albums) {
+
+    return `
+<main class="main">
+        <h2 class="album">Albums</h2>
+        <div class="allAlbums">
+            ${albums.map(album => {
+            return ` 
+                <article class="flip-card">
+                    <section class="inner">
+                        <div class="card-front">
+                            <img src="${album.imageUrl}" alt="Avatar" style="width:300px;height:300px;">
+                        </div>
+                        <section class="card-back">
+                            <h1 class="albumTitle">${album.title}</h1>
+                            <input type="hidden" class="id_field" value="${album.id}" > 
+                            <p class="recordLabel">${album.recordLabel}</p>
+                    </section>
+                </article>`
+            }).join("")
+            }
+        
+        </div>
+    </main>
+    `
+}
