5aaa7030e1b36fcfdb59cf51c64fb4e0cf4b48ca
AveryCS <Averydcs@gmail.com>
2022-03-23 15:23:16 -0400

    added in header and changed colors

--- frontend-spa/src/js/header.js
+++ frontend-spa/src/js/header.js
@@ -1,16 +1,10 @@
 export default function header(){
-   return  `<header class="header">
-    <h1 class="projectName">The Music Vault</h1>
+   return  `
+   
+   <header class="header">
     <nav class="nav">
-        <a class="nav-links" href="">
-            <button class="buttons contact">Contacts</button>
-        </a>
-        <a class="nav-links" href="">
-            <button class="buttons about">About</button>
-        </a>
-        <a class="nav-links" href="">
-            <button class="buttons songs">Songs</button>
-        </a>
+        <a class="nav-links" href="">About the Creators</a>
+        <a class="nav-links" href="">About WCCI</a>
     </nav>
 </header>`
 }
